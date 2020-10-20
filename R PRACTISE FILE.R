0d480ef4ac80d2e9e2d8b1082f3a8e8182f142ff
oghenerosiloko <silokodegreat@gmail.com>
2020-10-20 01:21:56 -0700

    new

---
+++ R PRACTISE FILE.R
@@ -0,0 +1,23 @@
+a <- 2L
+typeof(a)
+b <- 3.89
+typeof(b)
+fg <- 3+2i
+typeof(fg)
+
+A <- 109
+B<- 112
+C <- A+B
+D <- A-B
+Y <- A*B
+X<- A/B
+
+Ab <- "HELLO"
+name<-"GREAT"
+MESSAGE<- paste(Ab,name)
+MESSAGE
+print(MESSAGE)
+library(ggplot2)
+
+
+writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
